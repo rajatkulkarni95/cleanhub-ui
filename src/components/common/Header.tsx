720d8eada1cde24cb78b36948c7f6f2809d2964e
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-07-16 23:11:12 +0530

    Store access_token in session;

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,7 +4,6 @@ import { signIn, useSession } from "next-auth/react";
 const Header = () => {
   const { data: session } = useSession();
   const email = session?.user?.email;
-  console.log({ session });
   return (
     <header className="flex items-center justify-between flex-wrap bg-zinc-50 dark:bg-zinc-900 p-4">
       <h1 className="text-zinc-800 dark:text-zinc-100 text-3xl font-bold">
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,4 +11,19 @@ export default NextAuth({
       authorization: { params: { scope: "user:email delete_repo repo" } },
     }),
   ],
+  callbacks: {
+    jwt: async ({ token, account }) => {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      // Customize the session object
+      session.id = token.id;
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
